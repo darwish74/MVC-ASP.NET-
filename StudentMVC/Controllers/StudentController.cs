8518b9a571a5bc4d8df88d1699c7942930659f23
pc <pc@DESKTOP-NP1OA0M>
2024-07-20 15:48:09 +0300

    Add project files.

---
+++ StudentMVC/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentMVC.Models;
+
+namespace StudentMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+     StudentMockData sd=new StudentMockData();  
+     public IActionResult GetSTBYTD(int id)
+     {
+      Student student = sd.GetById(id);
+      return View("student",student);
+     }
+     public IActionResult GetALL(int id) 
+     {
+      List<Student> Lsd=sd.GetALL();
+      return View("students",Lsd);
+     }
+    }
+}
---
+++ StudentMVC/Models/StudentMockData.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentMVC.Models
+{
+    public class StudentMockData
+    {
+        public List<Student> students;
+        public StudentMockData()
+        {
+            students = new List<Student>(); 
+            students.Add(new Student() { Id=1,Name="Mohamed Ashraf",Address="10st almaadi",Image="1.jpg"});
+            students.Add(new Student() { Id=2,Name="Ahmed Ashraf",Address="15st Cairo",Image="2.jpg"});
+            students.Add(new Student() { Id=3,Name="Hassan ayman",Address="109st alex",Image="1.jpg"});
+        }
+        public Student GetById(int id) => students.FirstOrDefault(s => s.Id == id);
+        public List<Student> GetALL() => students;  
+    }
+}
